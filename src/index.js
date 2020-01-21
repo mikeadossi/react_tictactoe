81e2de641a486d71e2656e74161b9e58a15e1247
Mike Adossi <mikeadossi@gmail.com>
2020-01-20 18:13:28 -0800

    turn Square into a functional component

--- src/index.js
+++ src/index.js
@@ -6,11 +6,8 @@ class Square extends React.Component {
 
   render() {
     return (
-      <button
-        className="square"
-        onClick={() => this.props.onClick()}
-      >
-        {this.props.value}
+      <button className="square" onClick={props.onClick}>
+        {props.value}
       </button>
     );
   }
